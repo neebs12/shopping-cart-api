b6c2c6f11788d32535703878c7442f53fa2f769a
Jason Aricheta <jason.aricheta@gmail.com>
2023-01-22 18:31:47 +1300

    added "service" for fetching .json data

---
+++ data/events-service.js
@@ -0,0 +1,17 @@
+const eventsJSON = require("./events.json");
+
+// mocked async (ie: external service)
+// fetch event by id
+const fetchEventById = async (id) => {
+  const event = eventsJSON.find((event) => event.id === id);
+
+  if (event.length === 0) {
+    throw new Error(`Event of id:${id} not found`);
+  } else {
+    return event[0].price;
+  }
+};
+
+module.exports = {
+  fetchEventById,
+};
---
+++ data/ticket-types-service.js
@@ -0,0 +1,20 @@
+const ticketTypesJSON = require("./ticket-types.json");
+
+// mocked async (ie: external service)
+// fetch ticket by type and event_id
+const fetchTicketByTypeAndEventId = async (type, eventId) => {
+  console.log(ticketTypesJSON);
+  const ticket = ticketTypesJSON.find(
+    (ticket) => ticket.type === type && ticket.eventId === eventId
+  );
+
+  if (!ticket) {
+    throw new Error(`Ticket of type:${type} and id:${eventId} not found`);
+  } else {
+    return ticket;
+  }
+};
+
+module.exports = {
+  fetchTicketByTypeAndEventId,
+};
