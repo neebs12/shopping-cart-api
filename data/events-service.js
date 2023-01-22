9f09c8c937b3f3fc1e6a19376a7ba274198b2cb0
Jason Aricheta <jason.aricheta@gmail.com>
2023-01-23 06:13:06 +1300

    corrected events service bug

--- data/events-service.js
+++ data/events-service.js
@@ -4,11 +4,10 @@ const eventsJSON = require("./events.json");
 // fetch event by id
 const fetchEventById = async (id) => {
   const event = eventsJSON.find((event) => event.id === id);
-
   if (event.length === 0) {
     throw new Error(`Event of id:${id} not found`);
   } else {
-    return event[0].price;
+    return event;
   }
 };
 
