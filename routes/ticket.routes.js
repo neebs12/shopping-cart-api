45ebf1fbfcf990e0a8ae366437e88f9d325cf7ac
Jason Aricheta <jason.aricheta@gmail.com>
2023-01-22 19:59:05 +1300

    added pseudo code for ticket addition

--- routes/ticket.routes.js
+++ routes/ticket.routes.js
@@ -10,11 +10,49 @@ router.get("/", (req, res) => {
 });
 
 // add a ticket
+/*
+- When adding a ticket we must consider: 
+  - if the cart has reached the bookingLimit for the event assc to the ticket
+  - if the ticket is asc to an event of type "allocated", we must check if there are enough tickets left
+  - if the ticket is asc to an event of type "allocated", the seat_id must be checked for validity AND availability
+    - validity: the seat_id must be a valid seat_id for the event
+    - availability: for a ticket to be available, we need to fetch all existing tickets and see if the seat_id is already taken
+  - if the ticket is asc to an event of type != "allocated", the seat_id must be null, otherwise, return is an invalid ticket!
+
+process: 
+  INPUT: {type, event_id: eventId, seat_id: seatId(optional)} and cartID
+  0) initial information
+    - get related specific event and related ticket from cart
+    - const event = eventService.fetchEventById(eventId) 
+    - const cartTicketsForEvent = ticketDB.fetchTicketsByCartIdAndEventId(cartId, eventId)
+    - const globalTicketsForEvent = ticketDB.fetchTicketsByEventId(eventId)
+  1) address booking limit! 
+    - based on cartId, fetch all tickets this cart already has for the SPECIFIC event!
+    - if cartTicketsForEvent.length === event.bookingLimit, cannot add anymore tickets for event, then throw error
+    - addresses if this cart can add more tickets for itself
+  2) "allocated" and seatID availabilty
+    - get allocated seat ids const allocatedSeatIds = event.allocatedSeatIds,
+    - we want to find out which allocated seats are still available (we get a smaller collection) - const availableSeatIds = allocatedSeatIds.filter(seatId => !globalTicketsForEvent.some(ticket => ticket.seat_id === seatId))
+    - see if the seatId is in the availableSeatIds, if not, throw error
+    - this addresses if a seat is available across all carts (prevents double booking)
+  3) "generalAdmission" and globalLimit
+    - if event.type === "generalAdmission", then we need to check if the global limit has been reached
+    - if globalTicketsForEvent.length === event.globalLimit(which is 50), cannot add anymore tickets for event, then throw error
+  4) !"generalAdmission" and seatId
+    - if event.type !== "generalAdmission", then we need to check if the seatId is null
+    - if seatId !== null, then throw error
+
+  Later... now valid, the ticket can be added to the cart, therefore fetch price of the ticket. add it to the object to be added to the db!
+
+  Then... return the created id from the added ticket!
+*/
 router.post("/", async (req, res, next) => {
   try {
     const cartId = Number(res.locals.cartId);
     const { ticket } = req.body;
     const { type, event_id: eventId } = ticket;
+
+    // valid ticket! now add to cart!
     const { price } = await ticketTypesService.fetchTicketByTypeAndEventId(
       type,
       eventId
