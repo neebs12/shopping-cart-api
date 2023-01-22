841c01452bba4e85eaab15e4c3dfca3bf277507a
Jason Aricheta <jason.aricheta@gmail.com>
2023-01-22 19:57:35 +1300

    added preliminary tests

--- data/ticket-types-service.js
+++ data/ticket-types-service.js
@@ -3,7 +3,6 @@ const ticketTypesJSON = require("./ticket-types.json");
 // mocked async (ie: external service)
 // fetch ticket by type and event_id
 const fetchTicketByTypeAndEventId = async (type, eventId) => {
-  console.log(ticketTypesJSON);
   const ticket = ticketTypesJSON.find(
     (ticket) => ticket.type === type && ticket.eventId === eventId
   );
---
+++ server.test.js
@@ -0,0 +1,33 @@
+const app = require("./server.js");
+const supertest = require("supertest");
+const request = supertest(app);
+
+// test health from "/"
+it("GET /", async () => {
+  const response = await request.get("/");
+  expect(response.status).toBe(200);
+});
+
+// test unknown endpoint
+it("GET /unknown", async () => {
+  const response = await request.get("/unknown");
+  expect(response.status).toBe(404);
+});
+
+// valid cart but invalid urls after
+it("GET /cart/100/hello", async () => {
+  const response = await request.get("/unknown");
+  expect(response.status).toBe(404);
+});
+
+// test invalid cart id
+it("GET /cart/invalid", async () => {
+  const response = await request.get("/cart/999");
+  expect(response.status).toBe(404);
+});
+
+// test valid cart
+it("GET /cart/100", async () => {
+  const response = await request.get("/cart/100");
+  expect(response.status).toBe(200);
+});
